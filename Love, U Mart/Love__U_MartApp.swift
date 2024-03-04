d5d570cc7348a0ded261ce42263411a63a9094e5
xxu462 <94419600+xxu462@users.noreply.github.com>
2024-03-04 14:35:37 -0500

    Initial Commit

---
+++ Love, U Mart/Love__U_MartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love__U_MartApp.swift
+//  Love, U Mart
+//
+//  Created by 许歆业 on 2024-03-04.
+//
+
+import SwiftUI
+
+@main
+struct Love__U_MartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
